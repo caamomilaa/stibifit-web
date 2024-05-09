6c22220d4587aea2f1d930af77a6f8dd72dd47fd
caamomilaa <c.velazquezdamico@gmail.com>
2024-05-09 16:11:00 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+import iconMenu from '../assets/images/icon-menu.svg'
+const buttonElement = document.getElementById('button-menu');
+const menuElement = document.getElementById('menu');
+
+const handleMenu = () => {
+  if (menuElement.classList.contains('show')) {
+    menuElement.classList.remove('show');
+    buttonElement.src=iconMenu;
+    document.body.classList.remove('open')
+  } else {
+    menuElement.classList.add('show');
+    buttonElement.src=iconCloseMenu;
+    document.body.classList.add('open')
+  }
+};
+buttonElement.addEventListener('click', handleMenu);
